9572f1751284e6a90a87af93f0a316b36ccfe305
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-13 00:12:37 -0300

    Add react-icons and updates CountryPage with new data and Card component

--- pages/[country].js
+++ pages/[country].js
@@ -1,6 +1,7 @@
 import {
   Box, Divider, Flex, Grid, Text,
 } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
 import Card from '../components/card';
 
 import { getCountryData, getCountryHistoricalData } from './api/country';
@@ -20,11 +21,28 @@ export async function getServerSideProps({ params }) {
 
 const CountryPage = ({ countryData, countryHistoricalData }) => {
   const {
-    country, cases, deaths, todayCases, todayDeaths, recovered, tests, deathsPerOneMillion, countryInfo: { flag },
+    country, tests, countryInfo: { flag },
   } = countryData;
+
+  const { timeline: { cases, deaths, recovered } } = countryHistoricalData;
 
   console.log('Country Data: ', countryData);
   console.log('Country Data: ', countryHistoricalData);
+
+  // Difference in days to compare data
+  const difference = 1;
+
+  // Creates an array of the cases history
+  const casesArray = Object.entries(cases);
+  const casesTimeline = casesArray.map((item) => ({ date: item[0], value: item[1] }));
+
+  // Creates an array of the deaths history
+  const deathsArray = Object.entries(deaths);
+  const deathsTimeline = deathsArray.map((item) => ({ date: item[0], value: item[1] }));
+
+  // Creates an array of the recovered history
+  const recoveredArray = Object.entries(recovered);
+  const recoveredTimeline = recoveredArray.map((item) => ({ date: item[0], value: item[1] }));
 
   return (
     <Box p='8' bg='gray.100' height='100vh'>
@@ -45,21 +63,76 @@ const CountryPage = ({ countryData, countryHistoricalData }) => {
           </Box>
         </Flex>
         <Divider orientation='horizontal' my={4} />
+
+        {/** General */}
         <Text fontSize='xl' color='gray.700'>Dados Gerais</Text>
+        <Text fontSize='md' color='gray.700' fontWeight='light' mt={2}>Os dados percentuais representam a diferença em relação à medição da última semana</Text>
         <Grid templateColumns='repeat(4, 1fr)' gap={3} mt={4}>
-          <Card title='Casos' data={cases.toLocaleString()} color='cyan.400' />
-          <Card title='Mortes' data={deaths.toLocaleString()} color='cyan.400' />
-          <Card title='Recuperados' data={recovered.toLocaleString()} color='cyan.400' />
-          <Card title='Testes' data={tests.toLocaleString()} color='cyan.400' />
+          <Card title='Casos' value={casesTimeline[casesTimeline.length - 1].value} previousValue={casesTimeline[casesTimeline.length - 8].value} color='gray.200' />
+          <Card title='Mortes' value={deathsTimeline[deathsTimeline.length - 1].value} previousValue={deathsTimeline[deathsTimeline.length - 8].value} color='gray.200' />
+          <Card title='Recuperados' value={recoveredTimeline[casesTimeline.length - 1].value} previousValue={recoveredTimeline[casesTimeline.length - (difference + 1)].value} color='gray.200' />
+          <Card title='Testes' value={tests.toLocaleString()} color='gray.200' />
         </Grid>
-        <Text fontSize='xl' color='gray.700' mt={4}>Dados Diários</Text>
+
+        {/** Daily */}
+        <Text fontSize='xl' color='gray.700' mt={8}>Dados Diários</Text>
+        <Text fontSize='md' color='gray.700' fontWeight='light' mt={2}>Os dados percentuais representam a diferença em relação à medição do dia anterior</Text>
         <Grid templateColumns='repeat(4, 1fr)' gap={3} mt={4}>
-          <Card title='Casos' data={todayCases.toLocaleString()} color='purple.300' />
-          <Card title='Mortes' data={todayDeaths.toLocaleString()} color='purple.300' />
+          <Card
+            title='Casos'
+            value={casesTimeline[casesTimeline.length - 1].value - casesTimeline[casesTimeline.length - 2].value}
+            previousValue={
+              casesTimeline[casesTimeline.length - 2].value - casesTimeline[casesTimeline.length - 3].value
+            }
+            color='gray.200'
+          />
+          <Card
+            title='Mortes'
+            value={deathsTimeline[casesTimeline.length - 1].value - deathsTimeline[casesTimeline.length - 2].value}
+            previousValue={
+              deathsTimeline[casesTimeline.length - 2].value - deathsTimeline[casesTimeline.length - 3].value
+            }
+            color='gray.200'
+
+          />
         </Grid>
       </Box>
     </Box>
   );
+};
+
+CountryPage.propTypes = {
+  countryData: PropTypes.shape({
+    country: PropTypes.string,
+    tests: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    countryInfo: PropTypes.shape({
+      flag: PropTypes.string,
+    }),
+  }),
+  countryHistoricalData: PropTypes.shape({
+    timeline: PropTypes.shape({
+      cases: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      deaths: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      recovered: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }),
+  }),
+};
+
+CountryPage.defaultProps = {
+  countryData: {
+    country: '',
+    tests: null,
+    countryInfo: {
+      flag: '',
+    },
+  },
+  countryHistoricalData: {
+    timeline: {
+      cases: null,
+      deaths: null,
+      recovered: null,
+    },
+  },
 };
 
 export default CountryPage;
