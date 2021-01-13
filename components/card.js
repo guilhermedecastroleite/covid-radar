785baa7567b0cb6f306e02a6c98d21cfd7f3457a
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-13 00:12:12 -0300

    Adds new Card component

--- components/card.js
+++ components/card.js
@@ -1,12 +1,47 @@
-import { Box, Text } from '@chakra-ui/react';
+import {
+  Box, Flex, Icon, Text,
+} from '@chakra-ui/react';
+import { FaPlus, FaMinus } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
 const Card = ({
-  title, data, color, ...props
-}) => (
-  <Box bg={color} p='3' borderRadius='md' boxShadow='md' {...props}>
-    <Text fontSize='lg' color='gray.50'>{title}</Text>
-    <Text fontSize='2xl' color='gray.600'>{data}</Text>
-  </Box>
-);
+  title, value, previousValue, color, ...props
+}) => {
+  const current = parseInt(value, 10);
+  const previous = parseInt(previousValue, 10);
+  const percentage = ((current - previous) / current) * 100;
+
+  return (
+    <Flex bg={color} p='3' borderRadius='md' boxShadow='md' alignItems='flex-end' justifyContent='space-between' {...props}>
+      <Box>
+        <Text fontSize='lg' color='gray.600' fontWeight='light'>{title}</Text>
+        <Text fontSize='2xl' color='gray.600'>{value.toLocaleString()}</Text>
+      </Box>
+      {
+        previousValue
+        && (
+          <Flex alignItems='center'>
+            <Icon as={percentage > 0 ? FaPlus : FaMinus} color={percentage > 0 ? 'green.500' : 'blue.500'} />
+            <Text ml={1} color={percentage > 0 ? 'green.500' : 'blue.500'}>{`${Math.abs(percentage.toFixed(2))}%`}</Text>
+          </Flex>
+        )
+      }
+    </Flex>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  previousValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  color: PropTypes.string,
+};
+
+Card.defaultProps = {
+  title: '',
+  value: null,
+  previousValue: null,
+  color: null,
+};
 
 export default Card;
