42467ad7c19ffaacf67680f7c47d1fc2af1fbbf9
Guilherme Leite <guilhermedecastroleite@gmail.com>
2021-01-21 15:51:25 -0300

    Adds Combobox component

---
+++ components/Inputs/Combobox.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Box, Flex, Icon, Input, InputGroup, InputRightElement, List, ListItem,
+} from '@chakra-ui/react';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+import { useCombobox } from 'downshift';
+
+const ComboBox = ({
+  options, searchValue, onChange, inputProps, boxProps,
+}) => {
+  const [items, setItems] = useState(options);
+  const [value, setValue] = useState(searchValue);
+
+  const onValueChange = ({ inputValue }) => {
+    setValue(inputValue);
+    setItems(
+      options
+        .filter((item) => item.toLowerCase().startsWith(inputValue.toLowerCase())),
+    );
+  };
+
+  const {
+    isOpen,
+    getComboboxProps,
+    getInputProps,
+    getToggleButtonProps,
+    getMenuProps,
+    getItemProps,
+    highlightedIndex,
+    selectedItem,
+  } = useCombobox({
+    inputValue: value,
+    onInputValueChange: onValueChange,
+    items,
+  });
+
+  useEffect(() => {
+    onChange(value);
+  }, [onChange, value]);
+
+  return (
+    <Box {...boxProps}>
+      <Flex w='100%' justifyContent='center' {...getComboboxProps()}>
+        <InputGroup>
+          <Input {...getInputProps()} {...inputProps} />
+          <InputRightElement>
+            <Icon as={AiOutlineArrowDown} color='gray.500' cursor='pointer' {...getToggleButtonProps()} />
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+      <List
+        width='100%'
+        maxH='200px'
+        overflowY='scroll'
+        padding={isOpen ? 2 : 0}
+        styleType='disc'
+        bg='white'
+        border={isOpen ? '1px solid' : 'none'}
+        borderColor='teal.400'
+        borderRadius='md'
+        {...getMenuProps()}
+      >
+        {isOpen
+          && items.map((item, index) => (
+            <ListItem
+              fontWeight={selectedItem === item ? 'bold' : ''}
+              backgroundColor={highlightedIndex === index ? 'teal.400' : ''}
+              key={item}
+              {...getItemProps({ item, index })}
+            >
+              {item}
+            </ListItem>
+          ))}
+      </List>
+    </Box>
+  );
+};
+
+ComboBox.propTypes = {
+  options: PropTypes.array,
+  searchValue: PropTypes.string,
+  onChange: PropTypes.func,
+  inputProps: PropTypes.object,
+  boxProps: PropTypes.object,
+};
+
+ComboBox.defaultProps = {
+  options: [],
+  searchValue: '',
+  onChange: () => {},
+  inputProps: {},
+  boxProps: {},
+};
+
+export default ComboBox;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { Flex, Input, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import { getAllCountries } from './api/country';
 import GlobalTable from '../containers/Table/globalTable';
+import Combobox from '../components/Inputs/Combobox';
 
 export async function getServerSideProps() {
   const res = await getAllCountries();
@@ -16,6 +17,8 @@ export async function getServerSideProps() {
 
 const Home = ({ allCountriesData }) => {
   const router = useRouter();
+
+  const countriesList = allCountriesData.map((item) => item.country).sort();
 
   const showCountry = ({ country }) => {
     router.push(`/${country}`);
@@ -31,10 +34,22 @@ const Home = ({ allCountriesData }) => {
       <Text fontSize='xl' color='gray.700'>Visualize dados da Covid 19 ao redor do mundo.</Text>
       <Text mt={4} fontSize='lg' color='gray.700'>Todos os dados exibidos são consultados na base da Johns Hopkins University</Text>
 
-      <Input
+      {/* <Input
         mt={8}
         w='60%'
         placeholder='Busque por um país...'
+      /> */}
+
+      <Combobox
+        options={countriesList}
+        onChange={(value) => console.log(value)}
+        inputProps={{
+          placeholder: 'Busque por um país...',
+        }}
+        boxProps={{
+          mt: 8,
+          w: '60%',
+        }}
       />
 
       <GlobalTable
